34b0c4435a3338e91adf0edfbd23e654c3554b23
Pavel Safronov <prius@yandex-team.ru>
2016-12-03 18:22:29 +0300

    Added recursive realization of binary search, added some debug output, fixed problem with regular rezlization

--- cormen-introduction/binary-search.py
+++ cormen-introduction/binary-search.py
@@ -3,18 +3,45 @@ from random import randint
 x = sorted([ randint(0, 100) for i in xrange(0, 100) ])
 search = 32
 
-found = -1
-p = 0
-r = len(x) - 1
+print("x: {}".format(x))
+print("search: {}".format(search))
 
-while found == -1 and r > p:
-    q = p + (r - p) / 2
+
+def binary_search(x, search):
+    found = -1
+    p = 0
+    r = len(x) - 1
+
+    while found == -1 and r >= p:
+        q = (p + r) / 2
+        if x[q] == search:
+            found = q
+        if x[q] < search:
+            p = q + 1
+        elif x[q] > search:
+            r = q - 1
+
+    return found
+
+
+def recursive_binary_search(x, search, p=0, r=(len(x) - 1)):
+    q = (p + r) / 2
+    if p > r:
+        return -1
     if x[q] == search:
-        found = q
-    if x[q] < search:
-        p = q + 1
+        return q
+    elif x[q] < search:
+        return recursive_binary_search(
+            x, search,
+            (q + 1), r,
+        )
     elif x[q] > search:
-        r = q - 1
+        return recursive_binary_search(
+            x, search,
+            p, (q - 1),
+        )
 
-print x
-print found
+
+print binary_search(x, search)
+print recursive_binary_search(x, search)
+
