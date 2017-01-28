9943699d7a1dfadde1ce87becf8f81e5949d2511
Pavel Safronov <pv.safronov@gmail.com>
2017-01-28 22:23:01 +0300

    Added really simple sort for sorting m-dimension numbers

--- cormen-introduction/really-simple-sort.py
+++ cormen-introduction/really-simple-sort.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python2
 from random import randint
 
+m = 12
 x = [ randint(0, 1) for i in xrange(0, 100) ]
+print x
+x_m = [ randint(0, m) for i in xrange(0, 100) ]
+print x_m
 
-def really_simple_sort_py(x):
+
+def really_simple_sort(x):
     out = []
     for i in xrange(len(x)):
         if x[i] == 0:
@@ -14,4 +19,24 @@ def really_simple_sort_py(x):
 
     return out
 
-print really_simple_sort_py(x)
+
+def count_keys_equal(x, m):
+    equal = [0] * (m + 1)
+    for i in xrange(len(x)):
+        equal[x[i]] = equal[x[i]] + 1
+
+    return equal
+
+
+def count_keys_less(equal):
+    less = [0]
+    for i in xrange(1, len(equal) + 1):
+        less += [0]
+        less[i] += less[i - 1] + equal[i - 1]
+
+    return less
+
+
+print really_simple_sort(x)
+equal = count_keys_equal(x_m, m)
+print count_keys_less(equal)
