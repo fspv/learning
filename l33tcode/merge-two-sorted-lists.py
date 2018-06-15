d698e2965bd289d766267b578c8a7ad00b6997f6
Pavel Safronov <pv.safronov@gmail.com>
2018-06-16 00:28:32 +0300

    https://leetcode.com/problems/merge-two-sorted-lists/ solution

---
+++ l33tcode/merge-two-sorted-lists.py
@@ -0,0 +1,63 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        list_repr = []
+        list_node = self
+
+        while list_node != None:
+            list_repr.append(str(list_node.val))
+            list_node = list_node.next
+
+        return " -> ".join(list_repr)
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+        list_node_prev = None
+        result = None
+
+        while not (l1 is None and l2 is None):
+            try:
+                if l1.val > l2.val:
+                    add_node = l2
+                    l2 = l2.next
+                else:
+                    add_node = l1
+                    l1 = l1.next
+            except AttributeError:
+                if l1 is None:
+                    add_node = l2
+                    l2 = l2.next
+                elif l2 is None:
+                    add_node = l1
+                    l1 = l1.next
+
+            try:
+                list_node_prev.next = add_node
+                list_node_prev = list_node_prev.next
+            except AttributeError:
+                result = add_node
+                list_node_prev = result
+
+        return result
+
+
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(4)
+
+l2 = ListNode(1)
+l2.next = ListNode(3)
+l2.next.next = ListNode(4)
+
+solution = Solution()
+assert solution.mergeTwoLists(l1, l2).__repr__() == '1 -> 1 -> 2 -> 3 -> 4 -> 4'
