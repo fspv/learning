863c030fea387dbfb76bd23a8e2f3d3bfd8653c1
Safronov Pavel <prius@yandex-team.ru>
2018-12-25 16:50:08 +0300

    Added https://leetcode.com/problems/merge-intervals/ solution

---
+++ l33tcode/merge-intervals.py
@@ -0,0 +1,57 @@
+import unittest
+
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+    def __eq__(self, other):
+        return self.start == other.start and self.end == other.end
+
+
+class Solution(object):
+    def merge(self, intervals):
+        intervals.sort(key=lambda x: x.start)
+
+        result = []
+
+        if len(intervals):
+            result.append(intervals[0])
+
+        for interval in intervals:
+            if result[-1].end >= interval.start:
+                if result[-1].end < interval.end:
+                    result[-1].end = interval.end
+            else:
+                result.append(interval)
+
+        return result
+
+
+class TestSolution(unittest.TestCase):
+    def test_merge(self):
+        sol = Solution()
+
+        self.assertListEqual(sol.merge([]), [])
+        self.assertListEqual(sol.merge([Interval(0, 0)]), [Interval(0, 0)])
+        self.assertListEqual(
+            sol.merge([Interval(1, 10), Interval(3, 10)]),
+            [Interval(1, 10)]
+        )
+        self.assertListEqual(
+            sol.merge([Interval(1, 10), Interval(0, 0)]),
+            [Interval(0, 0), Interval(1, 10)]
+        )
+        self.assertListEqual(
+            sol.merge([Interval(1, 11), Interval(3, 10)]),
+            [Interval(1, 11)]
+        )
+        self.assertListEqual(
+            sol.merge([Interval(1, 10), Interval(0, 10)]),
+            [Interval(0, 10)]
+        )
+        self.assertListEqual(
+            sol.merge([Interval(1, 3), Interval(2, 6), Interval(8, 10), Interval(15, 18)]),
+            [Interval(1, 6), Interval(8, 10), Interval(15, 18)]
+        )
+        self.assertListEqual(sol.merge([Interval(1, 4), Interval(4, 5)]), [Interval(1, 5)])
