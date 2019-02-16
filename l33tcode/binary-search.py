3aa93007d4e792cd065e776f0a1626538e972c49
Pavel Safronov <pv.safronov@gmail.com>
2019-02-16 18:29:45 +0300

    Added better binary search https://leetcode.com/problems/binary-search/

--- l33tcode/binary-search.py
+++ l33tcode/binary-search.py
@@ -1,27 +1,73 @@
+import unittest
+
 class Solution:
     def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        x = nums
-        search = target
+        if not len(nums):
+            return -1
 
-        found = -1
-        p = 0
-        r = len(x) - 1
+        left, right = 0, len(nums) - 1
 
-        while found == -1 and r >= p:
-            q = int((p + r) / 2)
-            if x[q] == search:
-                found = q
-            if x[q] < search:
-                p = q + 1
-            elif x[q] > search:
-                r = q - 1
+        if nums[left] == target:
+            return left
 
-        return found
+        if nums[right] == target:
+            return right
 
-# TODO: write tests
-# TODO: speed it up
+        if nums[left] > target:
+            return -1
+
+        if nums[right] < target:
+            return -1
+
+        while right - left > 1:
+            center = int((right + left) / 2)
+
+            if nums[center] < target:
+                left = center
+            elif nums[center] > target:
+                right = center
+            else:
+                return center
+
+        return -1
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_empty(self):
+        self.assertEqual(self.sol.search([], 123), -1)
+
+    def test_one_non_matching(self):
+        self.assertEqual(self.sol.search([0], 123), -1)
+
+    def test_one_matching(self):
+        self.assertEqual(self.sol.search([123], 123), 0)
+
+    def test_out_of_boundaries_left(self):
+        self.assertEqual(self.sol.search([5, 6, 7, 8, 9, 10], 2), -1)
+
+    def test_out_of_boundaries_right(self):
+        self.assertEqual(self.sol.search([5, 6, 7, 8, 9, 10], 123), -1)
+
+    def test_not_in_array(self):
+        self.assertEqual(self.sol.search([5, 6, 7, 9, 10], 8), -1)
+
+    def test_leftmost_match(self):
+        self.assertEqual(self.sol.search([5, 6, 7, 9, 10], 5), 0)
+
+    def test_rightmost_match(self):
+        self.assertEqual(self.sol.search([5, 6, 7, 9, 10], 10), 4)
+
+    def test_central_match(self):
+        self.assertEqual(self.sol.search([5, 6, 7, 9, 10], 7), 2)
+
+    def test_match_custom1(self):
+        self.assertEqual(self.sol.search([5, 6, 7, 9, 10], 6), 1)
+
+    def test_match_custom2(self):
+        self.assertEqual(self.sol.search([5, 6, 7, 9, 10], 9), 3)
+
+if __name__ == "__main__":
+    unittest.main()
