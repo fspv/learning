2c2ded489429a915170828bbdfaffded7bd86d12
Pavel Safronov <pv.safronov@gmail.com>
2019-02-16 04:38:14 +0300

    Added DFS solution of https://leetcode.com/problems/number-of-islands/

--- l33tcode/number-of-islands.py
+++ l33tcode/number-of-islands.py
@@ -2,7 +2,46 @@ import unittest
 from collections import deque
 
 class Solution:
-    def numIslands(self, grid):
+    def numIslands(self, grid: 'List[List[str]]') -> 'int':
+        visited = set()
+        islands_count = 0
+
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                if (x, y) not in visited and int(grid[y][x]):
+                    islands_count += 1
+
+                    stack = [(x, y)]
+
+                    while len(stack):
+                        point = stack.pop()
+                        visited.add(point)
+
+                        if not int(grid[point[1]][point[0]]):
+                            continue
+
+                        neighbours = [
+                            (point[0] - 1, point[1]),
+                            (point[0] + 1, point[1]),
+                            (point[0], point[1] + 1),
+                            (point[0], point[1] - 1),
+                        ]
+
+                        for neighbour in neighbours:
+                            if neighbour[0] >= len(grid[0]) or \
+                                neighbour[1] >= len(grid) or \
+                                neighbour[0] < 0 or \
+                                neighbour[1] < 0:
+                                continue
+
+                            if neighbour not in visited:
+                                stack.append(point)
+                                stack.append(neighbour)
+                                break
+
+        return islands_count
+
+    def numIslandsBFS(self, grid):
         queue = deque()
         visited = {}
         islands = 0
