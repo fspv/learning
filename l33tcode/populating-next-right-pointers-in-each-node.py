a25fbcc76ad71a68c398070392b3286a29307087
Pavel Safronov <pv.safronov@gmail.com>
2019-03-12 01:15:08 +0300

    Added solution for https://leetcode.com/problems/populating-next-right-pointers-in-each-node/

---
+++ l33tcode/populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    def connect(self, node):
+        if node is None or not node.left and not node.right:
+            return node
+        if node.next:
+            node.right.next = node.next.left
+        node.left.next = node.right
+        self.connect(node.left)
+        self.connect(node.right)
+
+        return node
