860d6336c179c99e6dac010aa9d5a4eff72c05ab
Pavel Safronov <prius@yandex-team.ru>
2019-03-16 21:24:38 +0300

    Added recursive solution for https://leetcode.com/problems/reverse-linked-list/

--- l33tcode/reverse-linked-list.py
+++ l33tcode/reverse-linked-list.py
@@ -1,5 +1,27 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class Solution:
     def reverseList(self, head):
+        if head is None:
+            return None
+
+        head, last = self.reverseListRecursive(head, head)
+
+        return last
+
+    def reverseListRecursive(self, head, last):
+        if head.next is not None:
+            right, last = self.reverseListRecursive(head.next, head.next)
+            right.next = head
+            head.next = None
+
+        return head, last
+
+    def reverseListIteratively(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
@@ -14,3 +36,15 @@ class Solution:
             node = next_node
 
         return prev
+
+x = ListNode(1)
+x.next = ListNode(2)
+x.next.next = ListNode(3)
+x.next.next.next = ListNode(4)
+
+sol = Solution()
+y = sol.reverseList(x)
+print(y.val)
+print(y.next.val)
+print(y.next.next.val)
+print(y.next.next.next.val)
