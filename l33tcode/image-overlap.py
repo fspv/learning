02f032ba495f25d4f74f6d65a489b3ca72bf6d31
Pavel Safronov <pv.safronov@gmail.com>
2019-03-30 14:59:38 +0300

    A little bit faster bruteforce for https://leetcode.com/problems/image-overlap/

--- l33tcode/image-overlap.py
+++ l33tcode/image-overlap.py
@@ -7,14 +7,16 @@ class Solution:
         for row_offset in range(- len(A) + 1, len(A)):
             for col_offset in range(- len(A) + 1, len(A)):
                 overlap = 0
-                row_start = max(0, row_offset)
-                col_start = max(0, col_offset)
-                row_end = min(len(A), len(A) + row_offset)
-                col_end = min(len(A), len(A) + col_offset)
-                for A_row in range(row_start, row_end):
-                    for A_col in range(col_start, col_end):
-                        if A[A_row][A_col] == \
-                           B[A_row - row_offset][A_col - col_offset] == 1:
+                for A_row in range(
+                    max(0, row_offset),
+                    min(len(A), len(A) + row_offset)
+                ):
+                    for A_col in range(
+                        max(0, col_offset),
+                        min(len(A), len(A) + col_offset)
+                    ):
+                        if A[A_row][A_col] and \
+                           B[A_row - row_offset][A_col - col_offset]:
                             overlap += 1
                 max_overlap = max(overlap, max_overlap)
         return max_overlap
