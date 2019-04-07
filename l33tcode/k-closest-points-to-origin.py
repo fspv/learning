fd8186caf4c3abfafa8d0e3e60f56bf5479fb205
Pavel Safronov <pv.safronov@gmail.com>
2019-04-07 17:33:10 +0300

    Shorter version of https://leetcode.com/problems/k-closest-points-to-origin/

--- l33tcode/k-closest-points-to-origin.py
+++ l33tcode/k-closest-points-to-origin.py
@@ -3,7 +3,7 @@ import unittest
 
 class Solution:
     def kClosest(self, points, K):
-        return sorted(points, key=lambda point: point[0] * point[0] + point[1] * point[1])[:K]
+        return sorted(points, key=lambda point: point[0] ** 2 + point[1] ** 2)[:K]
 
 
 class TestSolution(unittest.TestCase):
