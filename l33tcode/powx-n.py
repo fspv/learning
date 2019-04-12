604782f60f84231825baaae957cfd2f8e5e80852
Safronov Pavel <pv.safronov@gmail.com>
2019-04-12 16:15:40 +0300

    Added solution for https://leetcode.com/problems/powx-n/

---
+++ l33tcode/powx-n.py
@@ -0,0 +1,49 @@
+class Solution:
+    def myPow(self, x, n):
+        def power(x, n):
+            if n == 1:
+                return x
+            elif n == 0:
+                return 1
+            else:
+                return power(x * x, n // 2) * power(x, n % 2)
+
+        if n >= 0:
+            return power(x, n)
+        elif n < 0:
+            return 1 / power(x, -n)
+
+
+class TestSolution:
+    def setup(self):
+        self.sol = Solution()
+
+    def test_zero1(self):
+        assert self.sol.myPow(0, 0) == 1
+
+    def test_zero2(self):
+        assert self.sol.myPow(0, 1) == 0
+
+    def test_zero3(self):
+        assert self.sol.myPow(0, 2) == 0
+
+    def test_one1(self):
+        assert self.sol.myPow(1, 0) == 1
+
+    def test_one2(self):
+        assert self.sol.myPow(1, 1) == 1
+
+    def test_one3(self):
+        assert self.sol.myPow(1, 4) == 1
+
+    def test_custom1(self):
+        assert self.sol.myPow(2, 10) == 1024
+
+    def test_custom2(self):
+        assert self.sol.myPow(2.1, 3) == 9.261000000000001
+
+    def test_custom3(self):
+        assert self.sol.myPow(2, -2) == 0.25
+
+    def test_custom4(self):
+        assert self.sol.myPow(2, 11) == 2048
