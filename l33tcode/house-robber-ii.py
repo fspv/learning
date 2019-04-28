aa5ccc15ecc6ceb49c8642622efbb595949b4dac
Pavel Safronov <pv.safronov@gmail.com>
2019-04-29 02:00:20 +0300

    Removed unnecessary variables, added tests https://leetcode.com/problems/house-robber-ii/

--- l33tcode/house-robber-ii.py
+++ l33tcode/house-robber-ii.py
@@ -1,28 +1,52 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
+    def rob(self, nums):
         if not nums:
             return 0
 
-        dp = dict()
+        if len(nums) == 1:
+            return nums[0]
 
         def rob_rec(start, end):
             if start >= end:
-                dp[(start, end)] = 0
-                return dp[(start, end)]
+                dp[start] = 0
+                return dp[start]
 
-            if (start, end) in dp:
-                return dp[(start, end)]
+            if start in dp:
+                return dp[start]
 
-            dp[(start + 2, end)] = rob_rec(start + 2, end)
-            dp[(start + 3, end)] = rob_rec(start + 3, end)
+            dp[start + 2] = rob_rec(start + 2, end)
+            dp[start + 3] = rob_rec(start + 3, end)
 
             return max(
-                (dp[(start + 2, end)] + nums[start]) if start < end else 0,
-                (dp[(start + 3, end)] + nums[start + 1]) if start + 1 < end else 0
+                (dp[start + 2] + nums[start]) if start < end else 0,
+                (dp[start + 3] + nums[start + 1]) if start + 1 < end else 0
             )
 
-        return max(
-            (rob_rec(2, len(nums) - 1) + nums[0]) if len(nums) > 0 else 0,
-            (rob_rec(3, len(nums)) + nums[1]) if len(nums) > 1 else 0,
-            (rob_rec(4, len(nums)) + nums[2]) if len(nums) > 2 else 0,
-        )
+        dp = {}
+        first = (rob_rec(0, len(nums) - 1))
+        dp = {}
+        second = (rob_rec(1, len(nums)))
+
+        return max(first, second)
+
+class TestSolution:
+    def setup(self):
+        self.sol = Solution()
+
+    def test_zero(self):
+        assert self.sol.rob([]) == 0
+
+    def test_one(self):
+        assert self.sol.rob([3]) == 3
+
+    def test_custom1(self):
+        assert self.sol.rob([1,2,3,1]) == 4
+
+    def test_custom2(self):
+        assert self.sol.rob([2,7,9,3,1]) == 11
+
+    def test_custom3(self):
+        assert self.sol.rob([2,1,2,1,1,2,1,2]) == 6
+
+    def test_custom4(self):
+        assert self.sol.rob([183,219,57,193,94,233,202,154,65,240,97,234,100,249,186,66,90,238,168,128,177,235,50,81,185,165,217,207,88,80,112,78,135,62,228,247,211]) == 3365
