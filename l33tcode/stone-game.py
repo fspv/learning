2cd150480d681605ec8ea08243558d0ee0fede5c
Pavel Safronov <pv.safronov@gmail.com>
2019-05-02 02:03:52 +0300

    Added DP solution for https://leetcode.com/problems/stone-game/

--- l33tcode/stone-game.py
+++ l33tcode/stone-game.py
@@ -1,3 +1,34 @@
+from functools import lru_cache
+
 class Solution:
-    def stoneGame(self, piles: List[int]) -> bool:
+    def stoneGame(self, piles):
+        @lru_cache(None)
+        def dp(left, right):
+            if left > right:
+                return 0
+
+            first = (right - left + len(piles)) % 2 == 1
+
+            if first:
+                return max(
+                    dp(left + 1, right) + piles[left],
+                    dp(left, right - 1) + piles[right]
+                )
+            else:
+                return min(
+                    dp(left + 1, right) - piles[left],
+                    dp(left, right - 1) - piles[right]
+                )
+
+        return dp(0, len(piles) - 1) > 0
+
+    def stoneGameMathematical(self, piles):
         return True
+
+
+class TestSolution:
+    def setup(self):
+        self.sol = Solution()
+
+    def test_custom1(self):
+        assert self.sol.stoneGame([1,2,3,4])
