5120a78e27be7b7f764c246c310be53d12398e36
Pavel Safronov <pv.safronov@gmail.com>
2019-12-16 19:23:57 +0000

    Added solution for https://leetcode.com/problems/n-queens/

---
+++ l33tcode/n-queens.py
@@ -0,0 +1,63 @@
+from typing import List
+
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        diag_pos_set = set()
+        diag_neg_set = set()
+        cols_set = set()
+
+        board = [[False] * n for _ in range(n)]
+        result = []
+
+        def place_queen(row):
+            if row == n:
+                result.append([
+                    "".join("Q" if c else "." for c in row)
+                    for row in board
+                ])
+            else:
+                for col in range(n):
+                    if not (
+                        row + col in diag_pos_set or \
+                        row - col in diag_neg_set or \
+                        col in cols_set
+                    ):
+                        diag_pos_set.add(row + col)
+                        diag_neg_set.add(row - col)
+                        cols_set.add(col)
+
+                        board[row][col] = True
+
+                        place_queen(row + 1)
+
+                        diag_pos_set.discard(row + col)
+                        diag_neg_set.discard(row - col)
+                        cols_set.discard(col)
+
+                        board[row][col] = False
+
+        place_queen(0)
+
+        return result
+
+
+class TestSolution:
+    def setup(self):
+        self.sol = Solution()
+
+    def test_case1(self):
+        assert self.sol.solveNQueens(4) == [
+            [
+                ".Q..",
+                 "...Q",
+                 "Q...",
+                 "..Q."
+            ],
+            [
+                "..Q.",
+                 "Q...",
+                 "...Q",
+                 ".Q.."
+            ]
+        ]
