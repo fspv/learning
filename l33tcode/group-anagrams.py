850003f35d4cc69acd8a9973da618e2304981ba9
Pavel Safronov <pv.safronov@gmail.com>
2020-04-06 21:38:39 +0100

    Added some more solutions and tests for https://leetcode.com/problems/group-anagrams/

--- l33tcode/group-anagrams.py
+++ l33tcode/group-anagrams.py
@@ -1,4 +1,34 @@
+import string
+from collections import defaultdict, Counter
+from typing import List
+
+
 class Solution:
+    def groupAnagramsCountMap(self, strs: List[str]) -> List[List[str]]:
+        str_map = defaultdict(list)
+
+        for word in strs:
+            counter = Counter()
+
+            for letter in word:
+                counter[string.ascii_lowercase.index(letter)] += 1
+
+            key = frozenset(counter.items())
+            str_map[key].append(word)
+
+        return list(str_map.values())
+
+    def groupAnagramsAlphabetMap(self, strs: List[str]) -> List[List[str]]:
+        str_map = defaultdict(list)
+        for word in strs:
+            str_hash = [0] * 26
+            for letter in word:
+                str_hash[string.ascii_lowercase.index(letter)] += 1
+
+            str_map[tuple(str_hash)].append(word)
+
+        return list(str_map.values())
+
     def groupAnagrams(self, strs):
         dict_with_rep = {}
         dict_without_rep = {}
@@ -30,3 +60,17 @@ class Solution:
             result.append(arr)
 
         return result
+
+
+
+
+class TestSolution:
+    def setup(self):
+        self.sol = Solution()
+
+    def test_case1(self):
+        self.sol.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) == [
+            ["ate", "eat", "tea"],
+            ["nat", "tan"],
+            ["bat"],
+        ]
