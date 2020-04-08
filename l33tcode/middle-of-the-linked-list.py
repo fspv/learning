d64e9b2159ec4932f856f8bb2554cc3c1916c5f0
Pavel Safronov <pv.safronov@gmail.com>
2020-04-08 17:55:10 +0100

    Added solution for https://leetcode.com/problems/middle-of-the-linked-list/

---
+++ l33tcode/middle-of-the-linked-list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+
+        middle = head
+        tail = head
+        count = 1
+
+        while tail:
+            if tail and tail.next and tail.next.next:
+                tail = tail.next.next
+            elif tail and tail.next:
+                return middle.next
+            elif tail:
+                return middle
+
+            middle = middle.next
