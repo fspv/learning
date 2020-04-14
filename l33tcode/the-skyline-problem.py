b5a2eed8e0068b434c79a07c45bf391510a78ec5
Pavel Safronov <pv.safronov@gmail.com>
2020-04-14 19:02:42 +0100

    Added solution for https://leetcode.com/problems/the-skyline-problem/

--- l33tcode/the-skyline-problem.py
+++ l33tcode/the-skyline-problem.py
@@ -5,22 +5,59 @@ class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
         def merge(left: List[List[int]], right: List[List[int]]) -> List[List[int]]:
             left_ptr, right_ptr = 0, 0
-            result = []
+            left_effective, right_effective = 0, 0
+            result: List[List[int]] = []
 
             while left_ptr < len(left) and right_ptr < len(right):
                 left_begin, left_height = left[left_ptr]
                 right_begin, right_height = right[right_ptr]
 
                 if left_begin < right_begin:
-                    right[right_ptr][1] = max(left_height, right_height)
-                    result.append(left[left_ptr])
+                    left_effective = left_height
+                    next_point = left[left_ptr][0]
                     left_ptr += 1
                 elif left_begin > right_begin:
+                    right_effective = right_height
+                    next_point = right[right_ptr][0]
+                    right_ptr += 1
+                else:
+                    left_effective = left_height
+                    right_effective = right_height
+                    next_point = right[right_ptr][0]
+                    left_ptr += 1
+                    right_ptr += 1
+
+                height = max(left_effective, right_effective)
+                if not result or (result and result[-1][1] != height):
+                    result.append([next_point, height])
+
+            return result + left[left_ptr:] + right[right_ptr:]
+
+        def divide(skylines: List[List[List[int]]]) -> List[List[int]]:
+            if len(skylines) == 1:
+                return skylines[0]
+
+            middle = len(skylines) // 2
+
+            left = divide(skylines[:middle])
+            right = divide(skylines[middle:])
+
+            return merge(left, right)
+
+        skylines = []
+
+        for left, right, height in buildings:
+            skylines.append([[left, height], [right, 0]])
+
+        return divide(skylines) if skylines else []
 
 
 class TestSolution:
     def setup(self):
         self.sol = Solution()
+
+    def test_empty(self):
+        assert self.sol.getSkyline([]) == []
 
     def test_case1(self):
         assert self.sol.getSkyline(
