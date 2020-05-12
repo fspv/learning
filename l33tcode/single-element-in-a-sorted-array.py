d44ac94890412d58061461b5a033cd3719bb9556
Pavel Safronov <pv.safronov@gmail.com>
2020-05-12 21:57:55 +0100

    Shorter solution for https://leetcode.com/problems/single-element-in-a-sorted-array/

--- l33tcode/single-element-in-a-sorted-array.py
+++ l33tcode/single-element-in-a-sorted-array.py
@@ -1,8 +1,8 @@
 func singleNonDuplicate(nums []int) int {
-    for pos, _ := range make([]int, len(nums) / 2 + 1) {
-        if ((pos * 2 + 1) < len(nums) && nums[pos * 2] != nums[pos * 2 + 1]) || (pos * 2 + 1) == len(nums) {
+    for pos, _ := range make([]int, len(nums) / 2) {
+        if nums[pos * 2] != nums[pos * 2 + 1] {
             return nums[pos * 2]
         }
     }
-    return 0
+    return nums[len(nums) - 1]
 }
