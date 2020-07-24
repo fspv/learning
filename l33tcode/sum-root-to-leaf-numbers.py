36096dceb3ff5199235f174fd5f75aac598b265d
Pavel Safronov <pv.safronov@gmail.com>
2020-07-24 21:55:15 +0100

    Added O(1) space solution for https://leetcode.com/problems/sum-root-to-leaf-numbers/

--- l33tcode/sum-root-to-leaf-numbers.py
+++ l33tcode/sum-root-to-leaf-numbers.py
@@ -6,6 +6,58 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: TreeNode) -> int:
+        # O(1) space solution
+
+        def find_predecessor(node):
+            path_length = 1
+            predecessor = node.left
+
+            while predecessor and predecessor.right and predecessor.right != node:
+                path_length += 1
+                predecessor = predecessor.right
+
+            return path_length, predecessor
+
+        def preorder_traversal(node):
+            total = 0
+            cur_sum = 0
+            while node:
+                path_length, predecessor = find_predecessor(node)
+                if predecessor and predecessor.right:
+                    # reset sum value
+                    if not predecessor.left:  # means leaf node
+                        total += cur_sum
+
+                    cur_sum = cur_sum // (10 ** path_length)
+
+                    # reset predecessor ptr and right count
+                    predecessor.right = None
+
+                    # go right or back
+                    node = node.right
+                elif predecessor and not predecessor.right:
+                    # visit
+                    cur_sum = cur_sum * 10 + node.val
+
+                    # set link and go left
+                    predecessor.right = node
+                    node = node.left
+                elif not predecessor:
+                    # visit
+                    cur_sum = cur_sum * 10 + node.val
+
+                    if not node.right:
+                        # we've reached the rightmost point
+                        total += cur_sum
+
+                    # go right or back
+                    node = node.right
+
+            return total
+
+        return preorder_traversal(root)
+
+    def sumNumbersONSpace(self, root: TreeNode) -> int:
         def dfs(node: TreeNode, prev: int) -> int:
             if not node:
                 return 0
