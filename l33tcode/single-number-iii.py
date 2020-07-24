8c6850e722d1e9b564549b0951aa8421957d7560
Pavel Safronov <pv.safronov@gmail.com>
2020-07-24 01:06:11 +0100

    Added solution for https://leetcode.com/problems/single-number-iii/

---
+++ l33tcode/single-number-iii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0  # a ^ b
+        total = sum(nums)
+        for num in nums:
+            xor ^= num
+
+        diff = xor & (-xor)
+
+        num1, num2 = 0, 0
+        for num in nums:
+            if num & diff > 0:
+                num1 ^= num
+            else:
+                num2 ^= num
+
+        return [num1, num2]
