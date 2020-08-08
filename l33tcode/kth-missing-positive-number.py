042f07d50b66a61fccbe955bf5c0ce02e268ff3d
Pavel Safronov <pv.safronov@gmail.com>
2020-08-08 17:17:23 +0100

    Added solution for https://leetcode.com/problems/kth-missing-positive-number/

---
+++ l33tcode/kth-missing-positive-number.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+def missing_between(arr: List[int], left: int, right: int) -> int:
+    if right >= len(arr):
+        return 0
+
+    if left == right:
+        return 0
+
+    return arr[right] - arr[left] + 1 - (right - left + 1)
+
+
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        if not arr:
+            return 0
+
+        if arr[0] > k:
+            return k
+
+        left, right = 0, len(arr) - 1
+
+        last_less = 0
+        missing_left = arr[0] - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+
+            missing = missing_between(arr, left, middle) + missing_left
+
+            if missing < k:
+                last_less = middle
+                left = middle + 1
+                missing_left = missing + missing_between(arr, middle, middle + 1)
+            else:
+                right = middle - 1
+
+        return k - (missing_between(arr, 0, last_less) + arr[0] - 1) + arr[last_less]
