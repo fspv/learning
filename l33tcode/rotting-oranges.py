28b4c528af5b3745f0eb0541380880c32e67cf63
Pavel Safronov <pv.safronov@gmail.com>
2020-08-09 12:54:20 +0100

    Added another solution for https://leetcode.com/problems/rotting-oranges/

--- l33tcode/rotting-oranges.py
+++ l33tcode/rotting-oranges.py
@@ -104,3 +104,46 @@ class Solution:
                     result = max(result, distances[row][col])
 
         return result
+
+    def orangesRottingBFS2(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0]) if grid else 0
+        queue = deque()
+        visited = set()
+        fresh = set()
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 2:
+                    queue.append((row, col, 0))
+                    visited.add((row, col))
+                    fresh.add((row, col))
+                if grid[row][col] == 1:
+                    fresh.add((row, col))
+
+        def neighbours(
+            row: int, col: int, rows: int, cols: int
+        ) -> Iterator[Tuple[int, int]]:
+            for neigh_row, neigh_col in (
+                (row + 1, col),
+                (row - 1, col),
+                (row, col - 1),
+                (row, col + 1),
+            ):
+                if 0 <= neigh_row < rows and 0 <= neigh_col < cols:
+                    if (neigh_row, neigh_col) not in visited and grid[neigh_row][
+                        neigh_col
+                    ] == 1:
+                        yield (neigh_row, neigh_col)
+
+        total = 0
+
+        while queue:
+            row, col, minutes = queue.popleft()
+            fresh.remove((row, col))
+            total = max(total, minutes)
+
+            for neigh_row, neigh_col in neighbours(row, col, rows, cols):
+                queue.append((neigh_row, neigh_col, minutes + 1))
+                visited.add((neigh_row, neigh_col))
+
+        return -1 if fresh else total
