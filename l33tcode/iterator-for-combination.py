8a71aa4aeb638e15f4b84f5ed5e658139c607254
Pavel Safronov <pv.safronov@gmail.com>
2020-08-13 09:37:59 +0100

    Added another solution for https://leetcode.com/problems/iterator-for-combination/

--- l33tcode/iterator-for-combination.py
+++ l33tcode/iterator-for-combination.py
@@ -1,4 +1,45 @@
+from typing import Tuple
+
+
 class CombinationIterator:
+
+    def __init__(self, characters: str, combinationLength: int):
+        self._stack = list(range(combinationLength))
+        self._length = combinationLength
+        self._chars = characters
+
+    def _next_char(self, pos) -> Tuple[bool, str]:
+        # return exists, next_char
+        if pos < len(self._chars) - 1:
+            return True, pos + 1
+        else:
+            return False, -1
+
+    def next(self) -> str:
+        result = "".join(map(lambda x: self._chars[x], self._stack))
+
+        while self._stack:
+            next_pos_exists, next_pos = self._next_char(self._stack[-1])
+
+            if next_pos_exists:
+                self._stack[-1] = next_pos
+                if len(self._stack) == self._length:
+                    break
+                self._stack.append(next_pos)
+            else:
+                self._stack.pop()
+
+        return result
+
+    def hasNext(self) -> bool:
+        return len(self._stack) > 0
+
+
+# Your CombinationIterator object will be instantiated and called as such:
+# obj = CombinationIterator(characters, combinationLength)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+class CombinationIteratorFirst:
 
     def __init__(self, characters: str, combinationLength: int):
         self._stack = [0]
