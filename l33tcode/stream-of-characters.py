6e035275a6fe961479dde1de6d9cb3dfea227c70
Pavel Safronov <pv.safronov@gmail.com>
2020-08-23 14:05:44 +0100

    Another solution for https://leetcode.com/problems/stream-of-characters/

--- l33tcode/stream-of-characters.py
+++ l33tcode/stream-of-characters.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Iterator
 
 
 class TrieNode:
@@ -26,7 +26,7 @@ def build_trie(words: List[str]) -> TrieNode:
     return trie_root
 
 
-class StreamChecker:
+class StreamCheckerForward:
     def __init__(self, words: List[str]) -> None:
         self._trie = build_trie(words)
         self._trie_ptrs = [self._trie]
@@ -44,6 +44,31 @@ class StreamChecker:
         self._trie_ptrs = trie_ptrs_new
 
         return result
+
+
+class StreamChecker:
+    def __init__(self, words: List[str]) -> None:
+        self._trie = build_trie(map(reversed, words))
+        self._stream = []
+
+    def _search_trie(self, stream: Iterator[str]) -> bool:
+        node = self._trie
+
+        for char in stream:
+            if node.end:
+                return True
+
+            if char in node.children:
+                node = node.children[char]
+            else:
+                break
+
+        return node.end
+
+    def query(self, letter: str) -> bool:
+        self._stream.append(letter)
+
+        return self._search_trie(reversed(self._stream))
 
 
 # Your StreamChecker object will be instantiated and called as such:
