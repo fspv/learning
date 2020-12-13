546ca068ca92a4c00f8c1eed53735e342cd9ee31
Pavel Safronov <pv.safronov@gmail.com>
2020-12-13 15:52:09 +0000

    Added solution for https://leetcode.com/problems/min-cost-to-connect-all-points/

---
+++ l33tcode/min-cost-to-connect-all-points.py
@@ -0,0 +1,37 @@
+import heapq
+from typing import List, Tuple
+
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        heap: List[Tuple[int, int]] = [(0, 0)]
+
+        visited = [False] * len(points)
+
+        distance = lambda point1, point2: (
+            abs(points[point1][0] - points[point2][0])
+            + abs(points[point1][1] - points[point2][1])
+        )
+
+        min_cost = 0
+        count = 0
+
+        while heap:
+            edge_weight, point = heapq.heappop(heap)
+
+            if visited[point]:
+                continue
+
+            visited[point] = True
+            min_cost += edge_weight
+            count += 1
+
+            if count == len(points):
+                break
+
+            for adj_point in filter(
+                lambda x: x != point and not visited[x], range(len(points))
+            ):
+                heapq.heappush(heap, (distance(point, adj_point), adj_point))
+
+        return min_cost
