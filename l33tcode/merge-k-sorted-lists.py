1e5c7cdf3e73258a098f3652e84e1468baa141f4
Pavel Safronov <pv.safronov@gmail.com>
2021-01-24 09:27:03 +0000

    Added another solution for https://leetcode.com/problems/merge-k-sorted-lists/

--- l33tcode/merge-k-sorted-lists.py
+++ l33tcode/merge-k-sorted-lists.py
@@ -4,6 +4,8 @@
 #         self.val = x
 #         self.next = None
 import heapq
+from typing import List, Tuple
+
 
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
@@ -33,4 +35,39 @@ class Solution:
             if min_node.next is not None:
                 heapq.heappush(heap, (min_node.next.val, count, min_node.next))
 
-        return root.next
+
+# Definition for singly-linked list.
+class ListNodeComparable:
+    def __init__(self, node: ListNode):
+        self.val = node.val
+        self.next = node.next
+        self.node = node
+
+    def __eq__(self, other: object) -> bool:
+        return self.val == other.val
+
+    def __lt__(self, other: object) -> bool:
+        return self.val < other.val
+
+
+class Solution2:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        heap: List[ListNodeComparable] = []
+
+        head = ListNode()
+
+        for node in lists:
+            if node:
+                heapq.heappush(heap, ListNodeComparable(node))
+
+        node = head
+        while heap:
+            cur_node = heapq.heappop(heap)
+
+            if cur_node.next:
+                heapq.heappush(heap, ListNodeComparable(cur_node.next))
+
+            node.next = cur_node.node
+            node = node.next
+
+        return head.next
