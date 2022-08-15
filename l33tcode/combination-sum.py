43686a42fd26656dbdac39c699a1ea9427ca0f99
Pavel Safronov <pv.safronov@gmail.com>
2022-08-15 19:09:39 +0100

    Added a better solution for https://leetcode.com/problems/combination-sum/

--- l33tcode/combination-sum.py
+++ l33tcode/combination-sum.py
@@ -1,8 +1,9 @@
 from functools import lru_cache
+from typing import List
+
 
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-
+    def combinationSum1(self, candidates: List[int], target: int) -> List[List[int]]:
         @lru_cache(None)
         def com_sum_rec(remain, pos):
             if remain == 0:
@@ -20,3 +21,35 @@ class Solution:
             return result
 
         return com_sum_rec(target, 0)
+
+    def combinationSum(
+        self, candidates: List[int], init_target: int
+    ) -> List[List[int]]:
+        stack: List[int] = []
+        result: List[List[int]] = []
+
+        def dfs(candidate: int, target: int) -> None:
+            if target == 0:
+                result.append(stack.copy())
+                return
+
+            if target < 0:
+                return
+
+            if candidate == len(candidates):
+                return
+
+            times = 1
+            while target - candidates[candidate] * times >= 0:
+                stack.append(candidates[candidate])
+                dfs(candidate + 1, target - candidates[candidate] * times)
+                times += 1
+
+            for _ in range(times - 1):
+                stack.pop()
+
+            dfs(candidate + 1, target)
+
+        dfs(0, init_target)
+
+        return result
