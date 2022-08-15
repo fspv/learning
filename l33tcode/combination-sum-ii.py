b065b34517b56004d01f23760eaaa481252a4673
Pavel Safronov <pv.safronov@gmail.com>
2022-08-15 19:29:45 +0100

    Added another solution for https://leetcode.com/problems/combination-sum-ii/

--- l33tcode/combination-sum-ii.py
+++ l33tcode/combination-sum-ii.py
@@ -3,6 +3,45 @@ from typing import List, Set, Tuple
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        stack: List[int] = []
+        result: List[List[int]] = []
+        candidates.sort()
+
+        print(candidates)
+
+        def dfs(pos: int, left: int) -> None:
+            print(pos, left)
+            if left == 0:
+                result.append(stack.copy())
+
+            if left < 0:
+                return
+
+            if pos == len(candidates):
+                return
+
+            repeated = 0
+
+            while pos + 1 < len(candidates) and candidates[pos] == candidates[pos + 1]:
+                pos += 1
+                repeated += 1
+
+            for times in range(repeated + 1):
+                stack.append(candidates[pos])
+                dfs(pos + 1, left - candidates[pos] * (times + 1))
+
+            for _ in range(repeated + 1):
+                stack.pop()
+
+            dfs(pos + 1, left)
+
+        dfs(0, target)
+
+        return result
+
+
+class Solution1:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         path: List[int] = []
         result: Set[Tuple[int, ...]] = set()
 
@@ -22,10 +61,7 @@ class Solution:
                 return
 
             for next_pos in range(pos, len(candidates)):
-                if (
-                    next_pos > pos
-                    and candidates[next_pos] == candidates[next_pos - 1]
-                ):
+                if next_pos > pos and candidates[next_pos] == candidates[next_pos - 1]:
                     continue
 
                 path.append(candidates[next_pos])
