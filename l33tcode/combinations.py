85a86b491762eea2491aafc25462467e71f38207
Pavel Safronov <pv.safronov@gmail.com>
2023-08-01 21:56:34 +0100

    Added another solution for https://leetcode.com/problems/combinations/

--- l33tcode/combinations.py
+++ l33tcode/combinations.py
@@ -1,5 +1,32 @@
+from typing import List
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        result: List[List[int]] = []
+        stack: List[int] = []
+
+        def combinations(num: int, left: int) -> None:
+            if left == 0:
+                if num == 0:
+                    result.append(stack.copy())
+                return
+
+            if num == 0:
+                return
+
+            stack.append(num)
+
+            for next_num in reversed(range(num)):
+                combinations(next_num, left - 1)
+
+            stack.pop()
+
+        for num in range(n):
+            combinations(num + 1, k)
+
+        return result
+
+    def combine1(self, n: int, k: int) -> List[List[int]]:
         def combinations(pos, kk):
             if pos + kk > n + 1:
                 return []
