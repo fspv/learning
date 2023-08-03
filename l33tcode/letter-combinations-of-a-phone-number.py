c795c9f578bd21dafebf9aa935416b9abac3a317
Pavel Safronov <pv.safronov@gmail.com>
2023-08-03 09:17:08 +0100

    Better solution for https://leetcode.com/problems/letter-combinations-of-a-phone-number/

--- l33tcode/letter-combinations-of-a-phone-number.py
+++ l33tcode/letter-combinations-of-a-phone-number.py
@@ -1,6 +1,8 @@
+from typing import List, Dict
+
 class Solution:
-    def letterCombinations(self, digits):
-        key_map = {
+    def letterCombinations(self, digits: str) -> List[str]:
+        key_map: Dict[str, str] = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -11,21 +13,23 @@ class Solution:
             "9": "wxyz",
         }
 
-        def guesses(pos):
-            if pos == len(digits):
-                return [""]
+        stack: List[str] = []
+        result: List[str] = []
 
-            result = []
+        def combinations(pos: int) -> None:
+            if pos == len(digits):
+                if stack:
+                    result.append("".join(stack))
+                return
 
             for letter in key_map[digits[pos]]:
-                for guess in guesses(pos + 1):
-                    result.append(letter + guess)
+                stack.append(letter)
+                combinations(pos + 1)
+                stack.pop()
 
-            return result
+        combinations(0)
 
-        result = guesses(0)
-
-        return result if len(result) > 1 else []
+        return result
 
 
 class TestSolution:
