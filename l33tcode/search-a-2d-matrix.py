d526be5781a878357597674d05b88c9567f5ab62
Pavel Safronov <pv.safronov@gmail.com>
2023-08-07 08:47:51 +0100

    Refactor https://leetcode.com/problems/search-a-2d-matrix/

--- l33tcode/search-a-2d-matrix.py
+++ l33tcode/search-a-2d-matrix.py
@@ -1,21 +1,22 @@
+from typing import List
+
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         if not matrix:
             return False
 
+        def get_value(pos: int):
+            row, col = pos // len(matrix[0]), pos % len(matrix[0])
+            return matrix[row][col]
+
         left, right = 0, len(matrix) * len(matrix[0]) - 1
 
-        while left <= right:
-            mid = (left + right) // 2
+        while left < right:
+            mid = left + (right - left) // 2
 
-            row = mid // len(matrix[0])
-            col = mid % len(matrix[0])
-
-            if matrix[row][col] < target:
+            if get_value(mid) < target:
                 left = mid + 1
-            elif matrix[row][col] > target:
-                right = mid - 1
-            else:
-                return True
+            elif get_value(mid) >= target:
+                right = mid
 
-        return False
+        return get_value(left) == target
