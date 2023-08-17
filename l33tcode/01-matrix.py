03f417bc39daf08e8404d2ddde364690515cdfec
Pavel Safronov <pv.safronov@gmail.com>
2023-08-17 09:07:38 +0100

    Refactor https://leetcode.com/problems/01-matrix/

--- l33tcode/01-matrix.py
+++ l33tcode/01-matrix.py
@@ -1,34 +1,34 @@
 import unittest
-import sys
+from typing import List, Set, Tuple
 
 
 class Solution:
-    def updateMatrix(self, matrix):
-        output = [[sys.maxsize - 100000 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(matrix), len(matrix[0]) if matrix else 0
+        dp = [
+            [rows * cols if matrix[row][col] else 0 for col in range(cols)]
+            for row in range(rows)
+        ]
 
-        for y in range(len(matrix)):
-            for x in range(len(matrix[0])):
-                if matrix[y][x] == 0:
-                    output[y][x] = 0
-                    continue
-                if x > 0:
-                    output[y][x] = min(output[y][x], output[y][x - 1] + 1)
-                if y > 0:
-                    output[y][x] = min(output[y][x], output[y - 1][x] + 1)
+        # Top-down, left-right
+        for row in range(rows):
+            for col in range(cols):
+                if col > 0:
+                    dp[row][col] = min(dp[row][col], dp[row][col - 1] + 1)
+                if row > 0:
+                    dp[row][col] = min(dp[row][col], dp[row - 1][col] + 1)
 
-        for y in range(len(matrix) - 1, -1, -1):
-            for x in range(len(matrix[0]) - 1, -1, -1):
-                if matrix[y][x] == 0:
-                    output[y][x] = 0
-                    continue
-                if x < len(matrix[0]) - 1:
-                    output[y][x] = min(output[y][x], output[y][x + 1] + 1)
-                if y < len(matrix) - 1:
-                    output[y][x] = min(output[y][x], output[y + 1][x] + 1)
+        # Bottom-up, right-left
+        for row in reversed(range(rows)):
+            for col in reversed(range(cols)):
+                if col < cols - 1:
+                    dp[row][col] = min(dp[row][col], dp[row][col + 1] + 1)
+                if row < rows - 1:
+                    dp[row][col] = min(dp[row][col], dp[row + 1][col] + 1)
 
-        return output
+        return dp
 
-    def updateMatrixBFS(self, matrix):
+    def updateMatrixBFS(self, matrix: List[List[int]]) -> List[List[int]]:
         output = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
 
         for y in range(len(matrix)):
@@ -37,12 +37,12 @@ class Solution:
                     continue
 
                 stack = [(y, x)]
-                visited = set()
+                visited: Set[Tuple[int, int]] = set()
                 level = 1
 
                 while stack:
                     stack_old = stack
-                    stack = []
+                    stack: List[Tuple[int, int]] = []
                     for new_y, new_x in stack_old:
                         visited.add((new_y, new_x))
 
@@ -52,9 +52,11 @@ class Solution:
                             (new_y + 1, new_x),
                             (new_y - 1, new_x),
                         ]:
-                            if 0 <= tmp_y < len(matrix) and \
-                               0 <= tmp_x < len(matrix[0]) and \
-                               not (tmp_y, tmp_x) in visited:
+                            if (
+                                0 <= tmp_y < len(matrix)
+                                and 0 <= tmp_x < len(matrix[0])
+                                and not (tmp_y, tmp_x) in visited
+                            ):
                                 if matrix[tmp_y][tmp_x] == 0:
                                     output[y][x] = level
                                     break
@@ -89,16 +91,28 @@ class TestSolution(unittest.TestCase):
         self.assertListEqual(self.sol.updateMatrix([[0, 1]]), [[0, 1]])
 
     def test_case1(self):
-        self.assertListEqual(self.sol.updateMatrix([[0,1,1],[1,1,1],[1,1,1]]), [[0,1,2],[1,2,3],[2,3,4]])
+        self.assertListEqual(
+            self.sol.updateMatrix([[0, 1, 1], [1, 1, 1], [1, 1, 1]]),
+            [[0, 1, 2], [1, 2, 3], [2, 3, 4]],
+        )
 
     def test_case2(self):
-        self.assertListEqual(self.sol.updateMatrix([[0,0,0],[0,1,0],[1,1,1]]), [[0,0,0],[0,1,0],[1,2,1]])
+        self.assertListEqual(
+            self.sol.updateMatrix([[0, 0, 0], [0, 1, 0], [1, 1, 1]]),
+            [[0, 0, 0], [0, 1, 0], [1, 2, 1]],
+        )
 
     def test_case3(self):
-        self.assertListEqual(self.sol.updateMatrix([[0,0,0],[0,0,0],[0,0,0]]), [[0,0,0],[0,0,0],[0,0,0]])
+        self.assertListEqual(
+            self.sol.updateMatrix([[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+        )
 
     def test_case4(self):
-        self.assertListEqual(self.sol.updateMatrix([[0,0,0],[0,1,0],[0,0,0]]), [[0,0,0],[0,1,0],[0,0,0]])
+        self.assertListEqual(
+            self.sol.updateMatrix([[0, 0, 0], [0, 1, 0], [0, 0, 0]]),
+            [[0, 0, 0], [0, 1, 0], [0, 0, 0]],
+        )
 
 
 if __name__ == "__main__":
