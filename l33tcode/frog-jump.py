084864c659c8ca037a2ff07d239efdcff1537e49
Pavel Safronov <pv.safronov@gmail.com>
2023-08-27 18:24:22 +0100

    Refactor https://leetcode.com/problems/frog-jump/

--- l33tcode/frog-jump.py
+++ l33tcode/frog-jump.py
@@ -1,18 +1,17 @@
-from functools import lru_cache
+from functools import cache
+from typing import List, Set
 
 
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
-        dp = [set() for _ in stones]
+        dp: List[Set[int]] = [set() for _ in stones]
         dp[0].add(0)
 
         stone_pos = {stone: pos for pos, stone in enumerate(stones)}
 
         for stone in range(len(stones)):
             for jump in dp[stone]:
-                for next_jump in filter(
-                    lambda j: j > 0, (jump - 1, jump, jump + 1)
-                ):
+                for next_jump in filter(lambda j: j > 0, (jump - 1, jump, jump + 1)):
                     next_stone = stones[stone] + next_jump
                     if next_stone in stone_pos:
                         dp[stone_pos[next_stone]].add(next_jump)
@@ -25,7 +24,7 @@ class Solution:
     def canCrossRecursive(self, stones: List[int]) -> bool:
         stone_pos = {stone: pos for pos, stone in enumerate(stones)}
 
-        @lru_cache(None)
+        @cache
         def dfs(stone: int, jump: int) -> bool:
             if stone + 1 == len(stones):
                 return True
