d4ab975147ad824d45a7e5d4a34020bd2812b116
Pavel Safronov <pv.safronov@gmail.com>
2023-09-03 08:57:03 +0100

    Refactor https://leetcode.com/problems/unique-paths/

--- l33tcode/unique-paths.py
+++ l33tcode/unique-paths.py
@@ -1,13 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        if m == 0 or n == 0:
+        rows, cols = n, m
+
+        if rows == 0 or cols == 0:
             return 0
 
-        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+        dp = [[0 for _ in range(cols + 1)] for _ in range(rows + 1)]
         dp[1][0] = 1
 
-        for row in range(n):
-            for col in range(m):
+        for row in range(rows):
+            for col in range(cols):
                 dp[row + 1][col + 1] = dp[row][col + 1] + dp[row + 1][col]
 
-        return dp[n][m]
+        return dp[rows][cols]
