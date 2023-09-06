82ffb11499d7045d79a5d0c9e0f96e122f7dab25
Pavel Safronov <pv.safronov@gmail.com>
2023-09-06 09:55:31 +0100

    Refactor https://leetcode.com/problems/split-linked-list-in-parts/

--- l33tcode/split-linked-list-in-parts.py
+++ l33tcode/split-linked-list-in-parts.py
@@ -1,79 +1,43 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+    val: int
+    next: Optional[ListNode] = None
+
 
 class Solution:
-    def splitListToParts(self, root, k):
-        def calculate_length(root):
-            if root is None:
-                return 0
+    def splitListToParts(
+        self, head: Optional[ListNode], k: int
+    ) -> List[Optional[ListNode]]:
+        def get_length(node: Optional[ListNode]) -> int:
+            return get_length(node.next) + 1 if node else 0
 
-            length = 1
-            while root.next:
-                length += 1
-                root = root.next
+        length: int = get_length(head)
+        result: List[Optional[ListNode]] = [None] * k
 
-            return length
+        part_length = length // k
+        extra = length % k
 
-        length = calculate_length(root)
-        result = [None for _ in range(k)]
+        node = head
 
-        remain = length % k
-        prev = None
+        for part in range(k):
+            result[part] = node
+            prev = node
 
-        for result_cell in range(k):
-            result[result_cell] = root
-            for _ in range(length // k):
-                if root.next:
-                    prev = root
-                    root = root.next
-                else:
-                    return result
+            for _ in range(part_length + (1 if extra > 0 else 0)):
+                # `node.next` is guaranteed to exist by the length calculations
+                # above
+                prev, node = node, node.next  # type: ignore
 
-            if remain == 0:
-                if prev:
-                    prev.next = None
-                continue
+            if extra > 0:
+                extra -= 1
 
-            remain -= 1
-            if root.next:
-                prev = root
-                root = root.next
-            else:
-                return result
-
-            prev.next = None
+            if prev:
+                prev.next = None
 
         return result
-
-
-# TODO: Tests are broken
-class TestSolution:
-    def setup(self):
-        self.sol = Solution()
-
-    def test_empty(self):
-        assert self.sol.splitListToParts(None, 5) == [[],[],[],[],[]]
-
-    def test_one_elem(self):
-        assert self.sol.splitListToParts(ListNode(1), 5) == [[1],[],[],[],[]]
-
-    def test_one(self):
-        l = ListNode(1)
-        l.next = ListNode(2)
-        l.next.next = ListNode(3)
-        assert self.sol.splitListToParts(l, 5) == [[1],[2],[3],[],[]]
-
-    def test_two(self):
-        l = ListNode(1)
-        l.next = ListNode(2)
-        l.next.next = ListNode(3)
-        l.next.next.next = ListNode(4)
-        l.next.next.next.next = ListNode(5)
-        l.next.next.next.next.next = ListNode(6)
-        l.next.next.next.next.next.next = ListNode(7)
-        l.next.next.next.next.next.next.next = ListNode(8)
-        l.next.next.next.next.next.next.next.next = ListNode(9)
-        l.next.next.next.next.next.next.next.next.next = ListNode(10)
-        assert self.sol.splitListToParts(l, 3) == [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]]
