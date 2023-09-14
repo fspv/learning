5959e5be3161599b28ff25319b4f376cc653f69e
Pavel Safronov <pv.safronov@gmail.com>
2023-09-14 19:40:38 +0100

    Refactor https://leetcode.com/problems/reconstruct-itinerary/

--- l33tcode/reconstruct-itinerary.py
+++ l33tcode/reconstruct-itinerary.py
@@ -1,21 +1,23 @@
-import heapq
+from __future__ import annotations
+
 from collections import deque
+from dataclasses import dataclass, field
+from typing import Deque, Dict, List
 
 
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        @dataclass
         class Airport:
-            def __init__(self, name):
-                self.name = name
-                self.destinations = []
+            name: str
+            destinations: List["Airport"] = field(default_factory=list)
 
-            def add_destination(self, dst):
+            def add_destination(self, dst: "Airport") -> None:
                 self.destinations.append(dst)
 
-        airports = {}
-        answer = deque()
+        airports: Dict[str, Airport] = {}
 
-        def create_airports(tickets):
+        def create_airports(tickets: List[List[str]]) -> None:
             for src, dst in tickets:
                 src_airport = airports.setdefault(src, Airport(src))
                 dst_airport = airports.setdefault(dst, Airport(dst))
@@ -24,58 +26,17 @@ class Solution:
             for airport in airports.values():
                 airport.destinations.sort(key=lambda x: x.name, reverse=True)
 
-        def dfs(airport):
+        create_airports(tickets)
+
+        answer: Deque[str] = deque()
+
+        def dfs(airport: Airport) -> None:
             while airport.destinations:
                 destination = airport.destinations.pop()
                 dfs(destination)
 
             answer.appendleft(airport.name)
 
-        create_airports(tickets)
         dfs(airports["JFK"])
-
-        return list(answer)
-
-    def findItineraryBacktrack(self, tickets: List[List[str]]) -> List[str]:
-        class Airport:
-            def __init__(self, name):
-                self.name = name
-                self.destinations = []
-                self.visited = []
-
-            def add_destination(self, dst):
-                self.destinations.append(dst)
-                self.visited.append(False)
-
-        airports = {}
-        answer = deque()
-
-        def create_airports(tickets):
-            for src, dst in tickets:
-                src_airport = airports.setdefault(src, Airport(src))
-                dst_airport = airports.setdefault(dst, Airport(dst))
-                src_airport.add_destination(dst_airport)
-
-            for airport in airports.values():
-                airport.destinations.sort(key=lambda x: x.name)
-
-        def backtrack(airport, path_length):
-            if path_length == len(tickets):
-                return True
-
-            for destination_pos, destination in enumerate(airport.destinations):
-
-                if not airport.visited[destination_pos]:
-                    airport.visited[destination_pos] = True
-                    if backtrack(destination, path_length + 1):
-                        answer.appendleft(destination.name)
-                        return True
-                    airport.visited[destination_pos] = False
-
-            return False
-
-        create_airports(tickets)
-        if backtrack(airports["JFK"], 0):
-            answer.appendleft("JFK")
 
         return list(answer)
