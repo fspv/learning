0b3df6c8055cb9562ed70ade84faa62a99666b40
Pavel Safronov <pv.safronov@gmail.com>
2023-09-16 08:26:23 +0100

    Refactor https://leetcode.com/problems/path-with-minimum-effort/

--- l33tcode/path-with-minimum-effort.py
+++ l33tcode/path-with-minimum-effort.py
@@ -1,7 +1,5 @@
 import heapq
-from typing import List, Tuple, Iterator
-from collections import defaultdict
-from functools import lru_cache
+from typing import Dict, List, Tuple, Iterator
 
 
 class Solution:
@@ -15,69 +13,27 @@ class Solution:
                 (row, col - 1),
                 (row, col + 1),
             ):
-                if (
-                    0 <= neigh_row < rows
-                    and 0 <= neigh_col < cols
-                ):
-                    yield (
-                        abs(heights[neigh_row][neigh_col] - heights[row][col]),
-                        neigh_row,
-                        neigh_col,
-                    )
+                if 0 <= neigh_row < rows and 0 <= neigh_col < cols:
+                    effort = abs(heights[neigh_row][neigh_col] - heights[row][col])
+                    yield effort, neigh_row, neigh_col
 
         heap = [(0, 0, 0)]
-        efforts = defaultdict(lambda: float("+inf"))
+        cell_to_effort: Dict[Tuple[int, int], int] = {}
+        min_effort = -1
 
         while heap:
             effort, row, col = heapq.heappop(heap)
-
             if (row, col) == (rows - 1, cols - 1):
-                return effort
+                min_effort = effort
+                break
 
             for neigh_effort, neigh_row, neigh_col in neighbors(row, col):
-                cur_effort = max(effort, neigh_effort)
-                if efforts[(neigh_row, neigh_col)] > cur_effort:
-                    efforts[(neigh_row, neigh_col)] = cur_effort
-                    heapq.heappush(heap, (cur_effort, neigh_row, neigh_col))
-
-    def minimumEffortPathBacktrack(self, heights: List[List[int]]) -> int:
-        rows, cols = len(heights), len(heights[0]) if heights else 0
-
-        visited = set()
-
-        def neighbors(row: int, col: int) -> Iterator[Tuple[int, int, int]]:
-            for neigh_row, neigh_col in (
-                (row - 1, col),
-                (row + 1, col),
-                (row, col - 1),
-                (row, col + 1),
-            ):
+                neigh_effort = max(effort, neigh_effort)
                 if (
-                    0 <= neigh_row < rows
-                    and 0 <= neigh_col < cols
-                    and (neigh_row, neigh_col) not in visited
+                    cell_to_effort.get((neigh_row, neigh_col), float("+inf"))
+                    > neigh_effort
                 ):
-                    yield (
-                        abs(heights[neigh_row][neigh_col] - heights[row][col]),
-                        neigh_row,
-                        neigh_col,
-                    )
+                    cell_to_effort[(neigh_row, neigh_col)] = neigh_effort
+                    heapq.heappush(heap, (neigh_effort, neigh_row, neigh_col))
 
-        def backtrack(row: int, col: int) -> int:
-            if (row, col) == (rows - 1, cols - 1):
-                return 0
-
-            min_effort = float("+inf")
-
-            visited.add((row, col))
-
-            for neigh_effort, neigh_row, neigh_col in neighbors(row, col):
-                min_effort = min(
-                    min_effort, max(backtrack(neigh_row, neigh_col), neigh_effort)
-                )
-
-            visited.remove((row, col))
-
-            return min_effort
-
-        return backtrack(0, 0)
+        return min_effort
