9dd32159580f16b79adb0a9a4f7c87cf2e120e85
Pavel Safronov <pv.safronov@gmail.com>
2023-09-29 09:11:19 +0100

    Refactor https://leetcode.com/problems/monotonic-array/

--- l33tcode/monotonic-array.py
+++ l33tcode/monotonic-array.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def isMonotonic(self, A: List[int]) -> bool:
         direction = A[0] <= A[-1]
@@ -19,16 +20,16 @@ class TestSolution:
         assert self.sol.isMonotonic([1])
 
     def test_case1(self):
-        assert self.sol.isMonotonic([1,2,2,3])
+        assert self.sol.isMonotonic([1, 2, 2, 3])
 
     def test_case2(self):
-        assert self.sol.isMonotonic([6,5,4,4])
+        assert self.sol.isMonotonic([6, 5, 4, 4])
 
     def test_case3(self):
-        assert not self.sol.isMonotonic([1,3,2])
+        assert not self.sol.isMonotonic([1, 3, 2])
 
     def test_case4(self):
-        assert self.sol.isMonotonic([1,2,4,5])
+        assert self.sol.isMonotonic([1, 2, 4, 5])
 
     def test_case5(self):
-        assert self.sol.isMonotonic([1,1,1])
+        assert self.sol.isMonotonic([1, 1, 1])
