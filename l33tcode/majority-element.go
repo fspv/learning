d6bb6289223ee59fec7b93ef0a8d545268d010e5
Pavel Safronov <pv.safronov@gmail.com>
2023-10-05 21:50:23 +0100

    More space-efficient solution for https://leetcode.com/problems/majority-element/

--- l33tcode/majority-element.go
+++ l33tcode/majority-element.go
@@ -1,12 +1,18 @@
+package main
+
 func majorityElement(nums []int) int {
-    count := map[int]int{}
+	candidate, count := 0, 0
 
-    for _, num := range nums {
-        count[num] += 1
-        if count[num] > len(nums) / 2 {
-            return num
-        }
-    }
+	for _, num := range nums {
+		if count == 0 {
+			candidate = num
+			count += 1
+		} else if num == candidate {
+			count += 1
+		} else {
+			count -= 1
+		}
+	}
 
-    return 0
+	return candidate
 }
