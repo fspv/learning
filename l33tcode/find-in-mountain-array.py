cd360e49a8b5cdab6d52a75d8169f2e81e75ac30
Pavel Safronov <pv.safronov@gmail.com>
2023-10-12 22:51:21 +0100

    Another solution for https://leetcode.com/problems/find-in-mountain-array/

--- l33tcode/find-in-mountain-array.py
+++ l33tcode/find-in-mountain-array.py
@@ -1,14 +1,66 @@
+from __future__ import annotations
+
 # """
 # This is MountainArray's API interface.
 # You should not implement it, or speculate about its implementation
 # """
-# class MountainArray:
-#    def get(self, index: int) -> int:
-#    def length(self) -> int:
+
+
+class MountainArray:
+    def get(self, index: int) -> int:
+        raise NotImplementedError()
+
+    def length(self) -> int:
+        raise NotImplementedError()
 
 
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: "MountainArray") -> int:
+        left, right = 0, mountain_arr.length() - 1
+        # Discard invalid parts as we search and track valid region here
+        next_left, next_right = left, right
+
+        # Search uphill (left) part
+        while left < right:
+            mid = left + (right - left) // 2
+
+            this_val = mountain_arr.get(mid)
+            next_val = mountain_arr.get(mid + 1)
+
+            # below on the left (uphill) slope
+            if this_val < next_val and this_val < target:
+                left = mid + 1
+            else:
+                right = mid
+
+            # discard uphill or above on the right (downhill) slope
+            if this_val < next_val or this_val > target:
+                next_left = max(next_left, mid + 1)
+            else:
+                next_right = min(next_right, mid)
+
+        # Check if found
+        if mountain_arr.get(left) == target:
+            return left
+
+        # Search the rest (not discarded earlier)
+        left, right = next_left, next_right
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if mountain_arr.get(mid) > target:
+                left = mid + 1
+            else:
+                right = mid
+
+        # Check if found
+        if mountain_arr.get(left) == target:
+            return left
+
+        return -1
+
+    def findInMountainArray2(self, target: int, mountain_arr: "MountainArray") -> int:
         def find_apex():
             left, right = 1, mountain_arr.length() - 2
 
