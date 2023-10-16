c58729a26ed811e67518850ad99ef21c260eb5b5
Pavel Safronov <pv.safronov@gmail.com>
2023-10-16 23:37:59 +0100

    Refactor https://leetcode.com/problems/pascals-triangle-ii/

--- l33tcode/pascals-triangle-ii.py
+++ l33tcode/pascals-triangle-ii.py
@@ -1,28 +1,19 @@
 import unittest
+from typing import List
 
 
 class Solution:
-    def getRow(self, rowIndex):
-        result = [None] * (rowIndex + 1)
-        mem = dict()
-        for pos in range(len(result)):
-            result[pos] = self.gen_cell(rowIndex, pos, mem)
-        return result
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
 
-    def gen_cell(self, level, pos, mem):
-        if pos == 0 or pos == level:
-            return 1
-        else:
-            result = 0
-            for prev_level, prev_pos in \
-                [(level - 1, pos - 1), (level - 1, pos)]:
-                if (prev_level, prev_pos) in mem:
-                    result += mem.pop((prev_level, prev_pos))
-                else:
-                    mem[(prev_level, prev_pos)] = \
-                        self.gen_cell(prev_level, prev_pos, mem)
-                    result += mem[(prev_level, prev_pos)]
-            return result
+        for _ in range(rowIndex):
+            next_row = [1] + [0] * (len(row) - 1) + [1]
+            for pos in range(1, len(next_row) - 1):
+                next_row[pos] = row[pos - 1] + row[pos]
+
+            row = next_row
+
+        return row
 
 
 class TestSolution(unittest.TestCase):
@@ -48,7 +39,42 @@ class TestSolution(unittest.TestCase):
         assert self.sol.getRow(5) == [1, 5, 10, 10, 5, 1]
 
     def test_six(self):
-        assert self.sol.getRow(33) == [1,33,528,5456,40920,237336,1107568,4272048,13884156,38567100,92561040,193536720,354817320,573166440,818809200,1037158320,1166803110,1166803110,1037158320,818809200,573166440,354817320,193536720,92561040,38567100,13884156,4272048,1107568,237336,40920,5456,528,33,1]
+        assert self.sol.getRow(33) == [
+            1,
+            33,
+            528,
+            5456,
+            40920,
+            237336,
+            1107568,
+            4272048,
+            13884156,
+            38567100,
+            92561040,
+            193536720,
+            354817320,
+            573166440,
+            818809200,
+            1037158320,
+            1166803110,
+            1166803110,
+            1037158320,
+            818809200,
+            573166440,
+            354817320,
+            193536720,
+            92561040,
+            38567100,
+            13884156,
+            4272048,
+            1107568,
+            237336,
+            40920,
+            5456,
+            528,
+            33,
+            1,
+        ]
 
 
 if __name__ == "__main__":
