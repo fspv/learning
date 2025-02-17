2943ca461f363680fbf9ee7c8b4909b95d23e479
Pavel Safronov <pv.safronov@gmail.com>
2025-02-17 22:48:23 +0000

    Added another solution for https://leetcode.com/problems/minimum-path-sum/

--- l33tcode/minimum-path-sum.py
+++ l33tcode/minimum-path-sum.py
@@ -1,5 +1,21 @@
 class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:
+    def minPathSum(self, grid: list[list[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        max_dp = sum(sum(r) for r in grid)
+        dp = [[max_dp] * cols for _ in range(rows)]
+        dp[0][0] = grid[0][0]
+
+        for row in range(rows):
+            for col in range(cols):
+                if 0 <= col - 1:
+                    dp[row][col] = min(dp[row][col], grid[row][col] + dp[row][col - 1])
+                if 0 <= row - 1:
+                    dp[row][col] = min(dp[row][col], grid[row][col] + dp[row - 1][col])
+
+        return dp[-1][-1]
+
+    def minPathSum2(self, grid: list[list[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         stack = set([(rows - 1, cols - 1)])
         dp = [[None] * cols for row in range(rows)]
@@ -12,8 +28,16 @@ class Solution:
             for row, col in old_stack:
                 dp[row][col] = float("+inf")
                 dp[row][col] = min(
-                    grid[row + 1][col] + dp[row + 1][col] if row + 1 < len(grid) else float("+inf"),
-                    grid[row][col + 1] + dp[row][col + 1] if col + 1 < len(grid[0]) else float("+inf"),
+                    (
+                        grid[row + 1][col] + dp[row + 1][col]
+                        if row + 1 < len(grid)
+                        else float("+inf")
+                    ),
+                    (
+                        grid[row][col + 1] + dp[row][col + 1]
+                        if col + 1 < len(grid[0])
+                        else float("+inf")
+                    ),
                 )
                 dp[row][col] = 0 if dp[row][col] == float("+inf") else dp[row][col]
                 if row - 1 >= 0:
