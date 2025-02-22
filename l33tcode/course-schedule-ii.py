df6990e3cb692251b032efec2c5c4e69acd3c442
Pavel Safronov <pv.safronov@gmail.com>
2025-02-22 21:01:38 +0000

    Added another solution for https://leetcode.com/problems/course-schedule-ii/

--- l33tcode/course-schedule-ii.py
+++ l33tcode/course-schedule-ii.py
@@ -1,8 +1,56 @@
+from collections import deque
 from enum import Enum
+from typing import Deque
 
 
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
+        def build_adj_list() -> list[list[int]]:
+            adj_list: list[list[int]] = [[] for _ in range(numCourses)]
+
+            for src, dst in prerequisites:
+                adj_list[src].append(dst)
+
+            return adj_list
+
+        def build_indegrees(adj_list: list[list[int]]) -> list[int]:
+            indegrees = [0] * numCourses
+
+            for dsts in adj_list:
+                for dst in dsts:
+                    indegrees[dst] += 1
+
+            return indegrees
+
+        adj_list = build_adj_list()
+        indegrees = build_indegrees(adj_list)
+
+        queue: Deque[int] = deque()
+
+        for pos, indegree in enumerate(indegrees):
+            if indegree == 0:
+                queue.append(pos)
+
+        result: list[int] = []
+
+        while queue:
+            pos = queue.popleft()
+            result.append(pos)
+
+            for next_pos in adj_list[pos]:
+                indegrees[next_pos] -= 1
+
+                if indegrees[next_pos] == 0:
+                    queue.append(next_pos)
+
+        if len(result) != numCourses:
+            return []
+
+        return list(reversed(result))
+
+    def findOrderDFS(
+        self, numCourses: int, prerequisites: list[list[int]]
+    ) -> list[int]:
         class Color(Enum):
             WHITE = 0
             GRAY = 1
@@ -16,7 +64,7 @@ class Solution:
 
         result = []
 
-        def dfs(course):
+        def dfs(course: int) -> bool:
             """
             @return False - loop, True - no loop
             """
@@ -44,8 +92,8 @@ class Solution:
         return result
 
     def findOrderStraighforward(
-        self, numCourses: int, prerequisites: List[List[int]]
-    ) -> List[int]:
+        self, numCourses: int, prerequisites: list[list[int]]
+    ) -> list[int]:
         courses = [[] for _ in range(numCourses)]
 
         for prerequisite in prerequisites:
@@ -54,7 +102,7 @@ class Solution:
         taken = set()
         result = []
 
-        def dfs(course, path):
+        def dfs(course: int, path: set[int]) -> bool:
             if course in path:
                 return False
 
