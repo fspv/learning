2aabf138bb3f0152b3dfbd3a7a5a99d09dd8decb
Pavel Safronov <pv.safronov@gmail.com>
2025-02-25 08:33:04 +0000

    Added another solution for https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/

--- l33tcode/number-of-sub-arrays-with-odd-sum.py
+++ l33tcode/number-of-sub-arrays-with-odd-sum.py
@@ -1,5 +1,19 @@
 class Solution:
-    def numOfSubarrays(self, arr: List[int]) -> int:
+    def numOfSubarrays(self, arr: list[int]) -> int:
+        prev_odd_subarrays = 0
+        total_odd_subarrays = 0
+
+        for pos, num in enumerate(arr):
+            if num % 2 == 0:
+                prev_odd_subarrays = prev_odd_subarrays
+            else:
+                prev_odd_subarrays = pos - prev_odd_subarrays + 1
+
+            total_odd_subarrays += prev_odd_subarrays
+
+        return total_odd_subarrays % (10 ** 9 + 7)
+
+    def numOfSubarrays2(self, arr: list[int]) -> int:
         odd, even = 0, 0
         result = 0
 
